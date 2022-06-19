5580fa68f2b7808dec641c4f25bd82291f962085
Jarno123 <jarno.bakker@hotmail.nl>
2022-06-19 22:30:01 +0200

    tailwind deploy 2

--- vedr-starter-template/tailwind.config.js
+++ vedr-starter-template/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./src/components/**/*.{js,jsx}", "./pages/index.js"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
+  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
